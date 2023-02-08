6ae48f62e7cc3ffc3aa23dce32db34c9103dadea
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-08 01:53:29 +0100

    Add files via upload

---
+++ SortingHat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    class SortingHat
+    {
+        String identity = "";
+        public SortingHat(String identity)
+        {
+            this.identity = identity;
+        }
+
+        enum Colleges
+        {
+            Gryffindor,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin
+        }
+
+        public void WhatCollege(SortingHat person)
+        {
+            if (person.identity == "intelligent")
+            {
+                Console.WriteLine("You are going to be a part of " + Colleges.Ravenclaw);
+            }
+            else if (person.identity == "brave")
+            {
+                Console.WriteLine("You are going to be a part of " + Colleges.Gryffindor);
+            }
+            else if (person.identity == "loyal")
+            {
+                Console.WriteLine("You are going to be a part of " + Colleges.Hufflepuff);
+            }
+            else
+                Console.WriteLine("You are going to be part of" + Colleges.Slytherin);
+        }
+    }
+}
